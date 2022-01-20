cd012b15aba05c5592cc2a839e74eccc5d6847df
“Wouter <woutermassie@gmail.com>
2022-01-20 21:50:26 +0100

    Laatste extratje

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Form from './component/form/form';
+import giraffe from './images/giraffe.png';
 function App() {
   return (
     <div className="App">
       
       <Form />
+      <img className='giraffe' src={giraffe} alt="giraffe" />
     </div>
   );
 }
